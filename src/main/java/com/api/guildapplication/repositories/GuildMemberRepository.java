1afe039bc55e50609a3bbdfcbec4edb8f59e829d
Natasha <natasharmsc@gmail.com>
2022-10-24 14:39:41 -0300

    deleting obsolete param

--- src/main/java/com/api/guildapplication/repositories/GuildMemberRepository.java
+++ src/main/java/com/api/guildapplication/repositories/GuildMemberRepository.java
@@ -2,8 +2,6 @@ package com.api.guildapplication.repositories;
 
 import com.api.guildapplication.models.GuildMemberModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,5 +11,5 @@ import java.util.UUID;
 public interface GuildMemberRepository extends JpaRepository<GuildMemberModel, UUID> {
 
     boolean existsByCharName(String charName);
-    Optional<GuildMemberModel> findByCharName(@Param("char_name")  String charName);
+    Optional<GuildMemberModel> findByCharName(String charName);
 }
